3ed325cee24c435deb78ddece279f93835eb8435
votre_email_github <votre_email_github>
2024-06-12 12:25:38 +0200

    2

--- front/lib/menuMagasin.js
+++ front/lib/menuMagasin.js
@@ -1,7 +1,7 @@
 import { fetchData } from "./fetch.js"
 import { superposerEcranGrid } from "./fonctionsEcran.js"
 
-export function menuMagasin(ecranActuel) {
+export function menuMagasin(ville) {
     // Fetch Sauvegarde et Objets
     fetchData('/sauvegarde').then((data) => {
         localStorage.setItem('sauvegarde', JSON.stringify(data))
@@ -45,7 +45,7 @@ export function menuMagasin(ecranActuel) {
 
     ecranMagasin.append(magasinArgentJoueur, magasinSelectionAction, magasinObjetIcone, magasinObjetNom, magasinObjetPrix, magasinObjetImage, magasinObjetDescription, magasinVendeurCharset, magasinVendeurDialogue, magasinBoxValidation)
 
-
+    // remplissage de la colonne de selection
     const argentJoueur = document.createElement('span')
     argentJoueur.innerText = `${sauvegarde.argent} GOLD`
     magasinArgentJoueur.append(argentJoueur)
@@ -55,13 +55,68 @@ export function menuMagasin(ecranActuel) {
     const optionSortir = document.createElement('span')
 
     optionAcheter.innerText = 'Acheter'
+    optionAcheter.id = 'optionAcheter'
     optionVendre.innerText = 'Vendre'
+    optionVendre.id = 'optionVendre'
     optionSortir.innerText = 'Sortir'
+    optionSortir.id = 'optionSortir'
 
     magasinSelectionAction.append(optionAcheter, optionVendre, optionSortir)
 
+    // remplissage la ligne du vendeur
     const vendeurCharset = document.createElement('img')
     vendeurCharset.src = './images/personnages/npc1.png'
     vendeurCharset.style.width = '100%'
     magasinVendeurCharset.append(vendeurCharset)
+
+    const vendeurDialogue = document.createElement('p')
+    vendeurDialogue.innerText = 'J\'ai plein d\'armes et d\'objets en stock ! Qu\'est ce qu\'il te faut ?'
+    magasinVendeurDialogue.append(vendeurDialogue)
+
+    // remplissage des objets
+    console.log(objetsInventaire)
+    optionAcheter.addEventListener('click', () => {
+
+    })
+    optionVendre.addEventListener('click', () => {
+        objetsInventaire.forEach((objet) => {
+            const objetIcone = document.createElement('img')
+            const objetNom = document.createElement('span')
+            const objetPrix = document.createElement('span')
+            const objetImage = document.createElement('img')
+            const objetDescription = document.createElement('p')
+
+            objetIcone.src = tabObjets[objet.id].image
+            objetIcone.id = 'objetIcone'
+            objetNom.innerText = tabObjets[objet.id].nom
+            objetNom.id = 'objetNom'
+            objetPrix.innerText = tabObjets[objet.id].prixVente
+            objetPrix.id = 'objetPrix'
+            
+            magasinObjetIcone.append(objetIcone)
+            magasinObjetNom.append(objetNom)
+            magasinObjetPrix.append(objetPrix)
+            magasinObjetImage.append(objetImage)
+            magasinObjetDescription.append(objetDescription)
+            
+            objetIcone.addEventListener('mouseenter', () => {
+                magasinObjetImage.style.display = 'flex'
+                magasinObjetDescription.style.display = 'flex'
+                ecranMagasin.id = 'ecranMagasinD'
+                objetImage.src = tabObjets[objet.id].image
+                objetImage.id = 'objetImage'
+                objetDescription.innerText = tabObjets[objet.id].description
+                })
+            objetIcone.addEventListener('mouseleave', () => {
+                ecranMagasin.id = 'ecranMagasin'
+                magasinObjetImage.style.display = 'none'
+                magasinObjetDescription.style.display = 'none'
+                objetImage.src = ''
+                objetDescription.innerText = ''
+            })
+
+
+
+        });
+    })
 }
